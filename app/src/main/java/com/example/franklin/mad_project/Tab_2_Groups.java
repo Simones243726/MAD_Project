9cb42b24c48880dc805d4a1b6097af1c997d1714
Davic15 <franklin.macias864@gmail.com>
2017-04-03 14:43:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/franklin/mad_project/Tab_2_Groups.java
@@ -0,0 +1,20 @@
+package com.example.franklin.mad_project;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by franklin on 02/04/17.
+ */
+
+public class Tab_2_Groups extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_2_groups, container, false);
+        return rootView;
+    }
+}
