953a5f8cd30fbb1e2d310c5276990e0ea0ef6b0e
Davic15 <franklin.macias864@gmail.com>
2017-04-03 21:55:14 +0200

    UI and List

--- app/src/main/java/com/example/franklin/mad_project/Tab_2_Groups.java
+++ app/src/main/java/com/example/franklin/mad_project/Tab_2_Groups.java
@@ -5,16 +5,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 
 /**
  * Created by franklin on 02/04/17.
  */
 
+
+
 public class Tab_2_Groups extends Fragment {
+
+    ListView listGroup;
+
+    public Tab_2_Groups(){
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_2_groups, container, false);
+
+        listGroup = (ListView) rootView.findViewById(R.id.listViewGroups);
+        ArrayList stringList = new ArrayList();
+            stringList.add("Group 1");
+            stringList.add("Group 2");
+            stringList.add("Group 3");
+            stringList.add("Group 4");
+
+
+        CustomAdapter adapter = new CustomAdapter (stringList, getActivity());
+        listGroup.setAdapter(adapter);
+
+
+
+
+
         return rootView;
     }
 }
