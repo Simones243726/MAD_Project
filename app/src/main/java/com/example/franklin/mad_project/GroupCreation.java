27b3e68482d1f89289f75d9f48db70767ad75d08
Davic15 <franklin.macias864@gmail.com>
2017-04-25 21:12:32 +0200

    UI and List 2

--- app/src/main/java/com/example/franklin/mad_project/GroupCreation.java
+++ app/src/main/java/com/example/franklin/mad_project/GroupCreation.java
@@ -1,8 +1,23 @@
 package com.example.franklin.mad_project;
 
+import android.*;
+import android.app.Activity;
+
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.ListView;
+import android.net.Uri;
+
+
 /**
  * Created by Frank on 4/21/2017.
  */
 
 public class GroupCreation {
+
 }
